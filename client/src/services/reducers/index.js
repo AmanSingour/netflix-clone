d41d838c38e07d6104f9ea92866d55546c88bde6
Aman Singour <amansigour15@gmail.com>
2021-05-25 20:17:35 +0530

    User reducer added

--- client/src/services/reducers/index.js
+++ client/src/services/reducers/index.js
@@ -1,7 +1,8 @@
 import {combineReducers} from 'redux'
+import { userReducer } from './user'
 
 export const reducers = combineReducers({
-    user: [],
+    user: userReducer,
     movies: [],
     favMovies: [],
     watchedMovies: [],
