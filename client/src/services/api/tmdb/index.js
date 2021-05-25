f7fc73c4ea4dc8c7d6c322f1f60f4f0328ad4866
Aman Singour <amansigour15@gmail.com>
2021-05-25 13:37:31 +0530

    Features exported

--- client/src/services/api/tmdb/index.js
+++ client/src/services/api/tmdb/index.js
@@ -8,21 +8,6 @@ export const BASEURL = 'https://api.themoviedb.org/3/'
 //? LANGUAGE PARAM...
 export const lang = '&language=en-US'
 
-//? FEATURE CONTAINS QUERY ROUTES...
-export const feature = {
-    disc_movie: {
-       QUERY: 'discover/movie',
-       QUERY_STRING: {
-           WITH_GENRE: '&with_genres=',
-           PAGE: '&page=',
-       }
-    },
-    genre_list: {
-        QUERY: 'genre/movie/list',
-    },
-    movie: {
-        QUERY: 'movie/',
-    }
-}
+export {feature as FEATURE} from './features'
 
 export const KeyWithLang = _key+lang
