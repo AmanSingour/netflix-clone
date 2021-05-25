dc4e36cf135db5ce0d73656f70c9eae4bdc54c6e
Aman Singour <amansigour15@gmail.com>
2021-05-25 20:19:40 +0530

    Signup page exported

--- client/src/layouts/index.js
+++ client/src/layouts/index.js
@@ -1,2 +1,3 @@
 export {default as LandingPage} from './LandingPage/LandingPage'
 export {default as PageNotFound} from './PageNotFound/PageNotfound'
+export {default as SignupPage} from './SignupPage/SignupPage'
