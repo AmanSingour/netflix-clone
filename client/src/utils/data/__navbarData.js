660ea36034235622fa9b3df8971ad3eadb78eaa0
Aman Singour <amansigour15@gmail.com>
2021-05-27 03:01:56 +0530

    Minor changes

--- client/src/utils/data/__navbarData.js
+++ client/src/utils/data/__navbarData.js
@@ -1,44 +1,42 @@
 import { _routes } from "./__routesData";
 
 export const _navWithAuth = [
-    {
-        label: 'Home',
-        title: 'Netflix Clone',
-        path: _routes.HOME,
-    },
-    {
-        label: 'Favorite',
-        title: 'Favorite Movies',
-        path: _routes.FAVMOVIES,
-    },
-    {
-        label: 'Watched',
-        title: 'Watched Movies',
-        path: _routes.WATCHED,
-    },
-    {
-        label: 'Profile',
-        tabs: [
-            {
-                label: 'My Profile',
-                title: 'Profile',
-                path: _routes.PROFILE,
-            },
-            {
-                label: 'Logout',
-                title: 'Logout',
-                path: _routes.LOGOUT,
-            },
-        ]
-    }
-]
+  {
+    label: "Home",
+    title: "Netflix Clone",
+    path: _routes.HOME,
+  },
+  {
+    label: "Favorite",
+    title: "Favorite Movies",
+    path: _routes.FAVMOVIES,
+  },
+  {
+    label: "Watched",
+    title: "Watched Movies",
+    path: _routes.WATCHED,
+  },
+  {
+    label: "Profile",
+    tabs: [
+      {
+        label: "My Profile",
+        title: "Profile",
+        path: _routes.PROFILE,
+      },
+      {
+        label: "Logout",
+        title: "Logout",
+        path: _routes.LOGOUT,
+      },
+    ],
+  },
+];
 
 export const _navData = [
-    {
-        label: 'Login',
-        title: 'Login',
-        path: _routes.LOGIN,
-    }
-]
-
-export default _navData
+  {
+    label: "Login",
+    title: "Login",
+    path: _routes.LOGIN,
+  },
+];
--- client/src/utils/data/index.js
+++ client/src/utils/data/index.js
@@ -1,3 +1,3 @@
 export {default as _routes} from './__routesData'
-export {default as _navData} from './__navbarData'
-export {default as _navWithAuth} from './__navbarData'
+export {_navData} from './__navbarData'
+export {_navWithAuth} from './__navbarData'
