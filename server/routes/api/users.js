d5de82f564fcacbf72925a5eb72dee77f9086bc7
Aman Singour <amansigour15@gmail.com>
2021-05-26 15:57:18 +0530

    Login Api response code changed

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,75 +1,78 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 //* USER MODEL FROM MODELS
-const User = require('../../models/User')
+const User = require("../../models/User");
 
-router.get('/test', (req, res) => res.send('user route testing!'))
+router.get("/test", (req, res) => res.send("user route testing!"));
 
 //! @route GET api/user
 //? @desc get all users
 // @access public
-router.get('/', (req, res) =>{
-    User.find()
-         .then(users => res.status(200).json({msg:users}))
-         .catch(err => res.status(404).json({userNotFound: err.message}))
+router.get("/", (req, res) => {
+  User.find()
+    .then((users) => res.status(200).json({ msg: users }))
+    .catch((err) => res.status(404).json({ userNotFound: err.message }));
 
-     //res.status(423).json({msg: 'Not accessible!'})
-})
+  //res.status(423).json({msg: 'Not accessible!'})
+});
 
 //! @route GET api/user/:id
 //? @desc get user by id
-router.get('/:id', (req, res) =>{
-    User.findById(req.params.id)
-        .then(user => res.json(user))
-        .catch(err => res.status(404).json({userNotFound: 'User not found!'}))
-})
+router.get("/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => res.json(user))
+    .catch((err) => res.status(404).json({ userNotFound: "User not found!" }));
+});
 
 //! @route POST api/user/login
-//? @desc Create new user
-router.post('/login', (req, res) =>{
-    User.findOne({ email: req.body.email})
-        .then(user => {
-            if(user === null) 
-                res.status(400).json({msg: 'User not found!'})
-            else
-                if(user.validPassword(req.body.password))
-                    res.status(201).json({msg: 'User Logged in!'})
-                else
-                    res.status(400).json({msg: 'Wrong Password!'})
-            })
-        .catch(err => res.status(400).json({error: err.message}))
-})
+//? @desc login user
+router.post("/login", (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (user === null) res.status(400).json({ msg: "User not found!" });
+      else if (user.validPassword(req.body.password))
+        res.status(200).json({
+          msg: "User Logged in!",
+          user: {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+          },
+        });
+      else res.status(400).json({ msg: "Wrong Password!" });
+    })
+    .catch((err) => res.status(400).json({ error: err.message }));
+});
 
 //! @route POST api/user/signup
 //? @desc Create new user
 // @access public
-router.post('/signup', (req, res) =>{
+router.post("/signup", (req, res) => {
+  var newUser = new User();
+  newUser.name = req.body.name;
+  newUser.email = req.body.email;
+  newUser.setPassword(req.body.password);
 
-    var newUser = new User()
-    newUser.name = req.body.name
-    newUser.email = req.body.email
-    newUser.setPassword(req.body.password)
-
-    User.create(newUser)
-        .then(user => res.status(201).json({msg: 'User added successfully!'}))
-        .catch(err => res.status(400).json({msg: 'User alredy exist'}))
-})
+  User.create(newUser)
+    .then((user) => res.status(201).json({ msg: "User added successfully!" }))
+    .catch((err) => res.status(400).json({ msg: "User alredy exist" }));
+});
 
 //! @route PUT api/users/:id
 //? @description update user
-router.put('/:id', (req, res) =>{
-    User.findByIdAndUpdate(req.params.id, req.body)
-        .then(user => res.json({msg: 'User data updated successfully!'}))
-        .catch(err => res.status(400).json({error: 'Unable to update data!'}))
-})
+router.put("/:id", (req, res) => {
+  User.findByIdAndUpdate(req.params.id, req.body)
+    .then((user) => res.json({ msg: "User data updated successfully!" }))
+    .catch((err) => res.status(400).json({ error: "Unable to update data!" }));
+});
 
 //! @route DELETE api/users/:id
 //? @description delete user
-router.delete('/:id', (req, res) =>{
-    User.findByIdAndRemove(req.params.id)
-        .then(user => res.status(200).json({msg: 'User deleted successfully!'}))
-        .catch(err => res.status(404).json({error: 'User not found!'}))
-})
+router.delete("/:id", (req, res) => {
+  User.findByIdAndRemove(req.params.id)
+    .then((user) => res.status(200).json({ msg: "User deleted successfully!" }))
+    .catch((err) => res.status(404).json({ error: "User not found!" }));
+});
 
-module.exports = router
+module.exports = router;
