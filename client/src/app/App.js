85501a9c61b971ca9968321767461bae16c04a5a
Aman Singour <amansigour15@gmail.com>
2021-05-27 02:56:36 +0530

    Login state action

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,27 +1,31 @@
 import axios from "axios";
 import React from "react";
-import { Container, Toast } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
+import { Toast } from "react-bootstrap";
+import { connect, useDispatch, useSelector } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Header } from "../components";
 import history from "../config/history";
 import Routes from "../routes";
+import { addFav, getFav } from "../services/actions/movie/fav-action";
 import { addGenre } from "../services/actions/movie/genre-action";
-import { removeNote } from "../services/actions/note/note-action";
+import { addNote, removeNote } from "../services/actions/note/note-action";
 
 //* IMPORTING TMDB API ROUTES...
 import { BASEURL, FEATURE, KeyWithRegion } from "../services/api/tmdb";
+
+//* IMPORTING SERVER BASEURL...
+import {BASEURL as ServerBase} from '../services/api/server'
 
 import { _navData, _navWithAuth } from "../utils/data";
 
 import styles from "./style.module.css";
 
-const App = (props) => {
+const App = ({ user }) => {
   const dispatch = useDispatch();
 
   const notify = useSelector((state) => state.note);
 
-  const isLoggedIn = useSelector((state) => state.user.loggedIn);
+  const loggedIn = user ? user.loggedIn : false;
 
   React.useEffect(() => {
     const query = BASEURL + FEATURE.genre_list.QUERY + KeyWithRegion;
@@ -35,18 +39,39 @@ const App = (props) => {
       });
   }, []);
 
+  React.useEffect(() => {
+    if(loggedIn){
+      axios.get(ServerBase+'movie/fav/'+user.user.id)
+      .then((res) => {
+        if (res.status === 200) {
+          dispatch(getFav(res.data.movie));
+        }
+      })
+      .catch((e) => {
+        if (e.response)
+          if (e.response.status === 400) dispatch(addNote(e.response.data.msg));
+          else dispatch(addNote(e.response.statusText));
+        else console.log(e);
+      });
+    }
+  }, [loggedIn]);
+
   return (
     <div className={styles.Container}>
       <Router history={history}>
         <header className={styles.Header}>
-          <Header data={isLoggedIn ? _navWithAuth : _navData} />
+          <Header
+            data={loggedIn ? _navWithAuth : _navData}
+            username={loggedIn && user.user.name}
+          />
         </header>
         <div>
           <div
             style={{
               position: "absolute",
-              right: 0,
+              right: 2,
               top: 100,
+              zIndex: 3,
             }}
           >
             {notify.map((note, index) => (
@@ -54,7 +79,7 @@ const App = (props) => {
                 key={index}
                 onClose={() => dispatch(removeNote(index))}
                 show={note ? true : false}
-                delay={6000}
+                delay={500}
                 autohide
               >
                 <Toast.Header>
@@ -67,7 +92,7 @@ const App = (props) => {
           </div>
           <Routes />
         </div>
-        <footer>
+        <footer style={{padding: "2rem 0", display: "block", textAlign: "center" }}>
           <p>Designed with ❤️ by Aman Singour</p>
         </footer>
       </Router>
@@ -75,4 +100,8 @@ const App = (props) => {
   );
 };
 
-export default App;
+const mapStateToProps = (state) => {
+  return { user: state.user };
+};
+
+export default connect(mapStateToProps)(App);
