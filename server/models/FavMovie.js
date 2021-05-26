2e9d4fba50cb5347862d97d6066e237dd5e45dd0
Aman Singour <amansigour15@gmail.com>
2021-05-27 02:55:32 +0530

    Favorite Movie Route build

--- server/models/FavMovie.js
+++ server/models/FavMovie.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 const FavMovieSchema = mongoose.Schema({
-    user_id: {type: String, ref: 'users'},
-    movie_id: String,
+    user_id: {type: String, ref: 'users', required: true},
+    movie_id: {type: String, required: true}
 })
 
 module.exports = FavMovie = mongoose.model('fav_movies', FavMovieSchema)
--- server/routes/api/fav.js
+++ server/routes/api/fav.js
@@ -7,25 +7,32 @@ const router = require('express').Router()
 //? GET ALL FAV MOVIES
 router.get('/:id', (req, res) =>{
     Movie.find({user_id: req.params.id})
-        .then(movie => res.status(200).json({msg: movie}))
+        .then(movie =>{ 
+            var data = movie.map(o => o.movie_id)
+            res.status(200).json({movie: data})
+        })
         .catch(err => res.status(400).json({error: err}))
 })
 
 //? ADD FAV MOVIES
-router.post('/:id', (req, res) =>{
+router.post('/', (req, res) =>{
     const newFavMovie = new Movie()
-    newFavMovie.user_id = req.params.id
-    newFavMovie.movie = req.body.movieID
+    newFavMovie.user_id = req.body.user_id
+    newFavMovie.movie_id = req.body.movie_id
+
     Movie.create(newFavMovie)
-        .then(movie => res.status(200).json({msg: `Movie added to favorite successfully!`}))
+        .then(movie => res.status(201).json({msg: `Movie added to favorite successfully!`}))
         .catch(err => res.status(400).json({error: err.message}))
 })
 
 
 
 //? DELETE FAV MOVIES
-router.delete('/:id', (req, res) =>{
-    Movie.findOneAndDelete({movie: req.params.id})
+router.delete('/', (req, res) =>{
+    Movie.findOneAndDelete({
+        movie_id: req.body.movie_id,
+        user_id: req.body.user_id
+    })
         .then(movie => res.status(200).json({msg: "Movie removed from favorite!"}))
         .catch(err => res.status(400).json({erro: err}))
 })
